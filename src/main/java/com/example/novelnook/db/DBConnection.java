8344582a8a6bc4db00c402df0b2df271741ba3cc
ireshdilsh <ireshdilshan46@gmail.com>
2024-11-03 19:31:33 +0530

    create DBConnection.java file singleton design pattern and complete it

--- src/main/java/com/example/novelnook/db/DBConnection.java
+++ src/main/java/com/example/novelnook/db/DBConnection.java
@@ -1,4 +1,23 @@
 package com.example.novelnook.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysq://localhost:3306/novelnook","root","1234");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return dbConnection == null ? new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
 }
